23a2d30714c76be907badcd90479928d20848547
Bas v.d. Wiel <bas@area536.com>
2022-09-13 13:49:33 +0200

    Use the Clap crate to accept --path argument on command line.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the disk image to create
+    #[clap(short, long)]
+    path: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Create file at: {}", args.path);
 }
