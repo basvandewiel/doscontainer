790a7bfac2f8253251b0edf569dedb6bdf7bd2ec
Bas v.d. Wiel <bas.van.de.wiel@enexis.nl>
2022-09-18 16:04:47 +0200

    Clarify comments

--- src/lib.rs
+++ src/lib.rs
@@ -168,7 +168,7 @@ impl CHS {
         let sectors_byte = BitVec::<_, Msb0>::from_element(bytes[1]);
         let cylinders_byte = BitVec::<_, Msb0>::from_element(bytes[2]);
 
-        // Put all those bits together into a Vec that's 24 bits ong
+        // Put all those bits together into a Vec that's 24 bits long
         let mut chs_bits: BitVec::<u8, bitvec::order::Msb0> = BitVec::new();
         chs_bits.extend_from_bitslice(&heads_byte);
         chs_bits.extend_from_bitslice(&sectors_byte);
@@ -178,23 +178,23 @@ impl CHS {
         let mut chs_heads: BitVec::<u8, bitvec::order::Msb0> = BitVec::new();
         chs_heads.extend_from_bitslice(&chs_bits[0..=7]);
 
-        // The sectors byte is only 6 bits long, so pad it with zeroes and fill it to 8.
+        // The sectors number is only 6 bits long, so pad it with zeroes to bring it up to 8 bits.
         let mut chs_sectors: BitVec::<u8, bitvec::order::Msb0> = BitVec::new();
         for i in [0..1] {
             chs_sectors.push(false);
         }
         chs_sectors.extend_from_bitslice(&chs_bits[9..=15]);
 
-        // The cylinders value is 10 bits long but we have room for 16, so pad with zeroes.
+        // The cylinders value is 10 bits long but a u16 has room for 16, so pad with zeroes.
         let mut chs_cylinders: BitVec::<u16, bitvec::order::Msb0> = BitVec::new();
         let mut i = 0;
         while i < 6 {
             chs_cylinders.push(false);
             i +=1;
         }
-        // Pull in the two bits stored in the sectors byte
+        // Pull in the two overflow bits that were stored in the sectors byte
         chs_cylinders.extend_from_bitslice(&chs_bits[8..=9]);
-        // Finish off with the actual cylinders byte itself
+        // Finish off by adding the actual cylinders byte itself
         chs_cylinders.extend_from_bitslice(&chs_bits[16..=23]);
 
         // Convert all these bits back to their numerical types and create the CHS struct
